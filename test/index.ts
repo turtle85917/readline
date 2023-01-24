d4d611a27232f734a2b1b9e199ef876e37fb9514
turtle85917 <siu85917@gmail.com>
2023-01-25 06:07:32 +0900

    Write type hints

--- test/index.ts
+++ test/index.ts
@@ -1,9 +1,10 @@
 import Readline from "../src";
 
 const readline = new Readline();
+readline.setPrompt("Input");
 readline.setAutoFocus(true);
 readline.addInputListener((data) => {
-  readline.write(`Recived : ${data}`);
+  readline.write(`Recived : ${data}\n`);
   if (data === "quit") process.exit();
 });
-readline.addCloseListener(() => console.log("\nEnd readline."))
+readline.addCloseListener(() => console.log("\nEnd readline."));
